cd5a1c0d8a1e92989875b29be73bfdd1f551e592
palmarytech <lvxudong@palmarytech.com>
2018-05-03 00:50:16 +0800

    initial

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#set( $SITE = "http://www.ilearning.com" )
+
+"""
+@version: ??
+@author: Dave
+@license: No
+@contact: dave-lvxudong@outlook.com
+@site: ${SITE}
+@software: ${PRODUCT_NAME}
+@file: ${NAME}.py
+@time: ${DATE} ${TIME}
+"""
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ ex001.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: Dave
+@license: No
+@contact: dave-lvxudong@outlook.com
+@site: http://www.ilearning.com
+@software: PyCharm
+@file: ex001.py
+@time: 2018/5/3 0:20
+"""
+
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
